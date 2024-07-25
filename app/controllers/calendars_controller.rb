76838d4ae22efe8070fef50bb45c8db88309aea6
takamasa-tech <g1012075@icloud.com>
2024-07-25 18:07:04 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -31,11 +31,17 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday = wdays[(@todays_date + x).wday] # 曜日情報を追加
+      wday_num = (@todays_date + x).wday
+
+      # wday_numが7以上になる場合に対応
+      if wday_num >= 7
+        wday_num = wday_num - 7
+      end
+
       days = {
         month: (@todays_date + x).month,
         date: (@todays_date + x).day,
-        wday: wday, # 曜日情報を追加
+        wday: wdays[wday_num], # 曜日情報を追加
         plans: today_plans
       }
       @week_days.push(days)
